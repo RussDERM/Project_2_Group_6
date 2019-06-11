1cb909c1ed9562a8881b2fbfb210c9166e6feced
Taylor Bradley <trbradley48@gmail.com>
2019-06-11 15:18:12 -0700

    added npm packages for twit and added code for twitter api call

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,3 +24,39 @@
 //     });
 //   });
 // };
+
+var Twit = require('twit');
+
+var trendTopic = '';
+var tweetId = 0;
+var T = new Twit({
+  consumer_key: 'q0FiTHT26bJxIjldXF8g2EDVw',
+  consumer_secret: 'WEBBW8erGtCBKA1gCDp8YCNpqTBLSkx3zwek0M0gdSE6gs3dYQ',
+  access_token: '1136480334088814592-KHSFeLe6UTjEuavcNvrtZHTCZBEMfM',
+  access_token_secret: 'zSpivL6RbF9pjCOisQqB5klBpB8fLC1S5iu27Sai1m1mv',
+  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL: true,     // optional - requires SSL certificates to be valid.
+})
+
+T.get('trends/place', { id: '2490383', count: 1 }, function (err, data, response) {
+  // console.log(data.statuses[0].text)
+  console.log(data[0].trends[0].name)
+  trendTopic = data[0].trends[0].name;
+  // console.log(trendTopic)
+  topicSearch(trendTopic)
+})
+
+function topicSearch(topic) {
+  T.get('search/tweets', { q: topic, count: 1 }, function (err, data, response) {
+    tweetId = data.statuses[0].id_str;
+    console.log(data)
+    // console.log(tweetId);
+    idSearch(tweetId)
+  })
+}
+
+function idSearch(input) {
+  T.get('statuses/lookup', { id: input , count: 1 }, function (err, data, response) {
+    console.log(data);
+  })
+}
