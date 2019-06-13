6a1969132fe02a75fe30f384b49609128b935f4b
CBirkbeck <cnbirkbeck@gmail.com>
2019-06-12 21:30:25 -0700

    added routes for logging in and out as well as route for google auth

---
+++ routes/auth-routes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+// authentication login
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+//authentication logout
+router.get("/logout", (req, res) => {
+    //handle with passport
+    res.send("logging out");
+});
+
+//authentication with google
+router.get("/google", (req, res) => {
+    //handle with passport
+    res.send("logging in with google");
+});
+
+//export the route to be imported in the index.js
+module.exports = router;
