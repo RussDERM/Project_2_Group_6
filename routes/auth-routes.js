e3b2bae64b4be436b6e266eeb4f28de2d1d38e89
CBirkbeck <cnbirkbeck@gmail.com>
2019-06-13 22:20:23 -0700

    added keys and callback URL in setup and modified routes for callback URL

---
+++ routes/auth-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+// authentication login
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+//authentication logout
+router.get("/logout", (req, res) => {
+    //handle with passport
+    res.send("logging out");
+});
+
+//authentication with google
+router.get("/google".passport.authenticate("google", {
+    scope: ["profile"]
+}));
+
+//callback route for google to redirect 
+router.get("/google/redirect", (req, res) => {
+    res.send("you've reached the callback URI")
+});
+
+//export the route to be imported in the server.js
+module.exports = router;
