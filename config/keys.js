f836e5293cd6db7af83d960bd4c9cde12980dc23
CBirkbeck <cnbirkbeck@gmail.com>
2019-06-14 21:14:03 -0700

    adding user models to include username and googleId

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//add file to .gitignore
+
+module.exports = {
+  google: {
+    clientID:
+      "762856790547-nc2alaqu84bdkcm28eedhvojaoac11b1.apps.googleusercontent.com",
+    clientSecret: "nGZhppYFicqp0OsdeoH-dekq"
+  }
+};
