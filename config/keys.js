15a4cc93225624034badc5736514bea51fe4975a
Julian Piron <46406565+Pironj@users.noreply.github.com>
2019-06-15 10:21:40 -0700

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-//add file to .gitignore
-
-module.exports = {
-  google: {
-    clientID:
-      "762856790547-nc2alaqu84bdkcm28eedhvojaoac11b1.apps.googleusercontent.com",
-    clientSecret: "nGZhppYFicqp0OsdeoH-dekq"
-  }
-};
