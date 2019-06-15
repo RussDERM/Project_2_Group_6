891be31b9abf5591fc57c33df1c80f80c47449ae
Julian Piron <pironj27@gmail.com>
2019-06-15 14:39:11 -0700

    fixed user log-in and log-out.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,23 +1,23 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys_google");
-const User = require("../models/user-models");
+const User = require("../models").User;
 
 //serialize user cookie set up
 passport.serializeUser((user,done) =>{
     //use the user id created in sequelize table to display if user is already logged in 
     //stores this user in a cookie
-    done(err, user.id)
+    done(null, user.id)
 });
 
 //deserialize take in an id to retrieve from cookie and see what user the id belongs to
 passport.deserializeUser((id,done) =>{
     User.findById(id).then((user) =>{
-        done(err, user.id);
+        done(null, user.id);
     }); 
    
 });
-
+console.log(User);
 //set up passport to use a google strategy
 passport.use(new GoogleStrategy({
     //options for the google strategy
@@ -26,7 +26,7 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/redirect",
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-}, (accessToken, refreshToken, profile, done) => {
+}, (request, accessToken, refreshToken, profile, done) => {
     // passport callback function goes here 
     //console.log("Passport callback function fired");
     //console.log(profile);
@@ -35,18 +35,18 @@ passport.use(new GoogleStrategy({
     User.findOne({ gooogleId: profile.id }).then((currentUser) => {
         if (currentUser) {
             //already have a user inside user collection with matching profile id returned from google
-            console.log("user is" + currentUser);
-            done(err, currentUser);
+            console.log("user is", currentUser);
+            done(null, currentUser);
         } else {
             // if not, create user in our db
 
             //create new user
             new User({
                 username: profile.displayName,
-                gooogleId: profile.id
+                googleId: profile.id
             }).save().then((newUser) => {
-                console.log("new user created" + newUser);
-                done(err, newUser);
+                console.log("new user created", newUser);
+                done(null, newUser);
                 
             });
         };
